dd0e8c6f2c3a336892e5538c280330be26207a73
Mahmodul7314 <mahmudulhasan7314@gmail.com>
2023-10-07 14:15:29 -0700

    tablet device responsive complete

--- src/components/DataCard/DataCard.jsx
+++ src/components/DataCard/DataCard.jsx
@@ -10,7 +10,7 @@ const DataCard = ({cardDatas}) => {
                <div><h2 className="text-4xl text-black font-bold px-8 mb-6">Our Survices</h2></div>
            <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1">
            {
-             cardDatas.map(cardData=> <DataCardsingle key={cardData.id} cardData={cardData}></DataCardsingle>) 
+             cardDatas.map(cardData=> <DataCardsingle key={cardData.id} cardData={cardData}></DataCardsingle>)
             }
            </div>
         </div>
